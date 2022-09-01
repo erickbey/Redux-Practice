94c481db1b1106d4936fbfeb38c935b6055060c2
Erick Bey <erickbey10@gmail.com>
2022-08-31 20:19:42 -0400

    added practice ecommerce redux site

---
+++ redux-ecommerce/src/app/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers } from "redux";
+
+import { inventoryReducer } from "../features/inventory/inventorySlice.js";
+import { cartReducer } from "../features/cart/cartSlice.js";
+import { currencyFilterReducer } from "../features/currencyFilter/currencyFilterSlice.js";
+import { searchTermReducer } from "../features/searchTerm/searchTermSlice.js";
+
+// Create and export the store here.
+export const store = createStore(
+  combineReducers({
+    cart: cartReducer,
+    inventory: inventoryReducer,
+    currencyFilter: currencyFilterReducer,
+    searchTerm: searchTermReducer
+  })
+);
